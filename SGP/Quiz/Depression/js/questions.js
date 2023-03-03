bfa86d297b1ecaba51f14ae8401534ce059e96e6
21ce147 <110080797+21ce147@users.noreply.github.com>
2023-03-03 12:27:26 +0530

    Add files via upload

---
+++ SGP/Quiz/Depression/js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "I feel overwhelmingly sad at times",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When I think of the future I feel hopeless",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+    {
+    numb: 3,
+    question: "I feel disappointed (even disgusted) with myself",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+    {
+    numb: 4,
+    question: "I am very interested in other people’s lives and like to listen to them",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+    {
+    numb: 5,
+    question: "I find it really hard to do anything, especially work",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+  {
+    numb: 6,
+    question: "My appetite has changed a lot",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+  {
+    numb: 7,
+    question: "My sleep patterns have been really disrupted",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+  {
+    numb: 8,
+    question: "I am very concerned, even preoccupied, with my physical health",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+  {
+    numb: 9,
+    question: "I feel irritated and annoyed by things in my life",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+  {
+    numb: 10,
+    question: "I think I am unattractive or ugly",
+    answer: "",
+    options: [
+      "Definitely agree",
+      "Slightly agree",
+      "Slightly diagree",
+      "Definitely disagree"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
